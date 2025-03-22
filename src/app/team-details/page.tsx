7591c5267d11b2dd65ec578929a579b8a5985ca8
codeftsaikat <soikatdey18@gmail.com>
2025-03-22 18:12:33 +0600

    Implement team-details page

---
+++ src/app/team-details/page.tsx
@@ -0,0 +1,12 @@
+import TeamDetailsView from "@/views/team-details";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <TeamDetailsView />
+    </div>
+  );
+};
+
+export default page;
