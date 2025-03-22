394212a31a7331ecdbabf564efeb66f6c7f1ed1d
codeftsaikat <soikatdey18@gmail.com>
2025-03-22 17:11:40 +0600

    created join team section under the team page

--- src/views/team/index.tsx
+++ src/views/team/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { TeamSection } from "./our-team";
+import JoinTeamSection from "./join-team";
 
 const TeamView = () => {
-  return <TeamSection />;
+  return (
+    <div>
+      <TeamSection />
+      <JoinTeamSection />
+    </div>
+  );
 };
 
 export default TeamView;
