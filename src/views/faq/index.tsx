e3b56663c63f56653eff10f2d6d9c89580e54be4
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-03-26 13:15:27 +0600

    feat: add FAQ section with accordion functionality, including FAQ data and contact form

--- src/views/faq/index.tsx
+++ src/views/faq/index.tsx
@@ -1,6 +1,8 @@
 import PageHeader from "@/components/common/page-header";
 import paths from "@/router/paths";
 import { TPage } from "@/types/cms/page";
+import BannerSection from "./banner-section";
+import ContactSection from "./contact-section";
 import FAQ from "./faq-section";
 
 type Props = {
@@ -21,7 +23,15 @@ const FaqView = (props: Props) => {
         ]}
         description={page.description}
       />
-      <FAQ />
+      <div className="grid lg:grid-cols-3 gap-y-20 gap-x-8 container py-[130px]">
+        <div className="lg:col-span-2">
+          <FAQ />
+        </div>
+        <div className="space-y-8">
+          <ContactSection />
+          <BannerSection />
+        </div>
+      </div>
     </>
   );
 };
