132b56242b3b9ff2a005c8ba8b3dd4cdc3bb188c
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-03-30 22:38:47 +0600

    Refactor ProjectDetailsPage and ProjectsGallery to use API for fetching project data; implement static params generation for dynamic routing

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,12 +1,38 @@
-import { projects } from "@/db/projects";
+import { getProject, getProjects } from "@/services/apis/project";
 import ProjectDetailsView from "@/views/project-details";
+import { notFound } from "next/navigation";
 
-const ProjectDetailsPage = () => {
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+const ProjectDetailsPage = async (props: Props) => {
+  const { slug } = await props.params;
+  const projectRes = await getProject(slug);
+
+  if (!projectRes.data) {
+    notFound();
+  }
+
   return (
     <>
-      <ProjectDetailsView project={projects[0]} />
+      <ProjectDetailsView project={projectRes.data} />
     </>
   );
 };
 
 export default ProjectDetailsPage;
+
+// ----------------------------------------------------------------------
+
+export async function generateStaticParams() {
+  const dataRes = await getProjects();
+
+  if (!dataRes.data) {
+    return [];
+  }
+
+  return dataRes.data.map((item) => ({
+    slug: item.slug,
+  }));
+}
