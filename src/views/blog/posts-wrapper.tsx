c43a1ac47dc30928be634d3a7b37dfd6912c39ce
codeftsaikat <soikatdey18@gmail.com>
2025-04-29 22:26:51 +0600

    Update blog section

--- src/views/blog/posts-wrapper.tsx
+++ src/views/blog/posts-wrapper.tsx
@@ -4,6 +4,7 @@ import img from "@/assets/404.png";
 import Image from "next/image";
 import Link from "next/link";
 import CommonMotionBox from "@/lib/motion/common-motion";
+import { ArrowRightIcon } from "lucide-react";
 
 type Props = {
   posts: TBlogPost[];
@@ -15,14 +16,15 @@ const PostsWrapper = (props: Props) => {
   return (
     <div>
       {!posts || posts.length === 0 ? (
-        <div className="flex flex-col items-center justify-center bg-black text-white py-28 px-4 space-y-2">
+        <div className="flex flex-col items-center justify-center bg-[#18191b] text-white py-28 px-4 space-y-2">
           <Image src={img} alt="404 Error" className="w-[200px] md:w-auto" />
           <p className="mt-4 text-lg font-bold font-sans text-heading-4 text-center">
             The blog you are looking for doesn’t exist
           </p>
           <Link href="/blog">
-            <button className="mt-6 px-6 py-3 bg-primary text-black text-heading-5 font-semibold rounded-full transition cursor-pointer">
-              Back to Blog
+            <button className="mt-6 px-6 py-3 bg-primary text-black text-heading-6 font-semibold rounded-full transition cursor-pointer flex items-center gap-1">
+              Back To The Blog
+              <ArrowRightIcon size={20} />
             </button>
           </Link>
         </div>
