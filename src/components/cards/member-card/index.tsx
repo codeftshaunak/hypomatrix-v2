a5acc4502747b97f8c120019638a96e04dbf0427
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-05-02 10:26:37 +0600

    feat: implement MemberCard component in TeamCarousel for improved member display

--- src/components/cards/member-card/index.tsx
+++ src/components/cards/member-card/index.tsx
@@ -1,20 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import paths from "@/router/paths";
 import type { TMember } from "@/types/cms/team";
 import { Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import SocialIcon from "../common/social-icon";
+import SocialIcon from "../../common/social-icon";
 
 type Props = {
   data: TMember;
+  isActive?: boolean;
 };
 
 const MemberCard = (props: Props) => {
-  const { data } = props;
+  const { data, isActive } = props;
 
   return (
-    <article className="group cursor-pointer relative">
+    <article
+      className={cn(
+        "group cursor-pointer relative duration-300",
+        isActive === false ? "opacity-50" : ""
+      )}
+    >
       <div className="aspect-[3/4] rounded-lg overflow-hidden relative mb-4 ">
         <Image
           src={data.avatar.url}
@@ -26,7 +33,7 @@ const MemberCard = (props: Props) => {
         {/* Social Links */}
         <div className="absolute top-3 right-3 group/social focus-within:z-10 flex flex-col gap-2 z-50">
           <Button size="icon" variant="secondary" className="relative z-10">
-            <Plus />
+            <Plus className="group-hover/social:rotate-45 duration-200" />
           </Button>
 
           {data.socials.map((item, index) => {
@@ -44,7 +51,16 @@ const MemberCard = (props: Props) => {
           })}
         </div>
       </div>
-      <div className="absolute left-1/2 -translate-x-1/2 flex-col items-center justify-center text-center bg-black/40 backdrop-blur-xl bottom-5 sm:bottom-0 max-w-[80%] w-full py-7 md:py-10 rounded-2xl sm:opacity-0 group-hover:opacity-100 group-hover:bottom-5 duration-300 px-[10px]">
+      <div
+        className={cn(
+          `absolute left-1/2 -translate-x-1/2 flex-col items-center justify-center text-center bg-black/40 backdrop-blur-xl bottom-10 max-w-[80%] w-full py-7 rounded-2xl  duration-300 px-[10px]`,
+          isActive === undefined
+            ? "sm:opacity-0 sm:bottom-0 group-hover:opacity-100 group-hover:bottom-10"
+            : "",
+          isActive === true ? `opacity-100 bottom-5` : ``,
+          isActive === false ? `bottom-0 opacity-0` : ``
+        )}
+      >
         <h3 className="font-bold">
           <Link
             href={paths.team.details(data.slug)}
@@ -53,7 +69,9 @@ const MemberCard = (props: Props) => {
             {data.name}
           </Link>
         </h3>
-        <p className="text-sm text-gray-300 line-clamp-1">{data.role}</p>
+        <p className="text-sm text-muted-foreground line-clamp-1">
+          {data.role}
+        </p>
       </div>
     </article>
   );
--- src/views/home/sections/carousel.tsx
+++ src/views/home/sections/carousel.tsx
@@ -2,7 +2,7 @@
 import { CarouselApi } from "@/components/ui/carousel";
 import { useEffect, useState } from "react";
 
-import CarouselCard from "@/components/carousel";
+import MemberCard from "@/components/cards/member-card";
 import SectionHeader from "@/components/common/section-header";
 import {
   Carousel,
@@ -52,7 +52,7 @@ const TeamCarousel = (props: Props) => {
         />
 
         <Carousel opts={{ loop: true, align: "center" }} setApi={setApi}>
-          <CarouselContent className="mb-16 min-h-[500px] items-center ml-0">
+          <CarouselContent className="mb-16 items-center ml-0">
             {members.map((member, index) => (
               <CarouselItem
                 onClick={() => {
@@ -60,12 +60,9 @@ const TeamCarousel = (props: Props) => {
                   api?.scrollTo(index);
                 }}
                 key={member.id}
-                className={cn(
-                  index === current ? `z-10 px-4` : `z-0 px-2`,
-                  "sm:basis-1/2 md:basis-1/3 lg:basis-1/4 2xl:basis-1/5"
-                )}
+                className={cn("sm:basis-1/2 md:basis-1/3 lg:basis-1/4")}
               >
-                <CarouselCard data={member} isActive={index === current} />
+                <MemberCard data={member} isActive={index === current} />
               </CarouselItem>
             ))}
           </CarouselContent>
