81fe9e04b9169317e150ec6134cc9c815b7bc8dc
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-05-02 09:44:23 +0600

    refactor: remove hardcoded address from footer component

--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -64,7 +64,6 @@ export function Footer(props: Props) {
                     Main Address
                   </h3>
                   <p>{website.addresses[0]}</p>
-                  <p>Bangladesh</p>
                 </div>
               )}
 
