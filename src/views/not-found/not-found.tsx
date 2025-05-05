852a16e2a0aa3eb2471d3437e3edad99866414db
codeftsaikat <soikatdey18@gmail.com>
2025-05-05 12:26:32 +0600

    Update not found page

--- src/views/not-found/not-found.tsx
+++ src/views/not-found/not-found.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import img from "@/assets/404.png";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center bg-black text-white py-28 px-4 space-y-2">
+    <div className="flex flex-col items-center justify-center py-28 px-4 space-y-2 h-screen">
       <Image src={img} alt="404 Error" className="w-[200px] md:w-auto" />
-      <p className="mt-4 text-lg font-bold font-sans text-heading-4 text-center">
+      <p className="mt-4 text-lg font-bold font-sans text-heading-4 text-center mb-10">
         The page you are looking for doesn’t exist
       </p>
       <Link href="/">
-        <button className="mt-6 px-6 py-3 bg-primary text-black text-heading-5 font-semibold rounded-full transition cursor-pointer">
-          Back to Home
-        </button>
+        <Button size={"xl"}>Back to Home</Button>
       </Link>
     </div>
   );
